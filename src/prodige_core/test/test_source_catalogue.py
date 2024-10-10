4cc288a6e99fbd975abd2eb3562e443c8dcd3dc4
Jaime Pineda <jaime.e.pineda@gmail.com>
2024-10-10 22:48:19 +0200

    update test ' -remove support for python 3.7 (not supported in CI) -fixed small inconsistencies with test after updating catalogue '

--- src/prodige_core/test/test_source_catalogue.py
+++ src/prodige_core/test/test_source_catalogue.py
@@ -17,7 +17,7 @@ def test_validate_source_id_source() -> None:
 def test_get_outflow_information_number_source() -> None:
     list_source_info = prodige_core.source_catalogue.get_outflow_information()
     # test that there are 16 sources in the region dictionary
-    assert len(list_source_info[0]) == 16
+    assert len(list_source_info[0]) == 22
 
 
 def test_get_outflow_information_first_last_sources() -> None:
@@ -25,13 +25,13 @@ def test_get_outflow_information_first_last_sources() -> None:
     # print(source_name[0])
     # test that the first source is 'IRS3A'
     assert (sources_outflowPA[0] == 'IRS3A') and (
-        sources_outflowPA[-1] == 'B1-bS')
+        sources_outflowPA[-1] == 'IRAS4C')
 
 
 def test_get_region_names_first_last_sources() -> None:
     source_name = prodige_core.source_catalogue.get_region_names()
     # test that the first source is 'IRS3A'
-    assert (source_name[0] == 'L1448N') and (source_name[-1] == 'B1-bS')
+    assert (source_name[0] == 'L1448N') and (source_name[-1] == 'SVS13B')
 
 
 def test_get_region_names_number_source() -> None:
